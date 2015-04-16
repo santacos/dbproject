0709b9cf779b664adcf408bbf1a194905b954132
santacos <santacos_lala@hotmail.com>
2015-04-16 23:16:12 +0700

    first commit yeah!

---
+++ app/Services/Registrar.php
@@ -0,0 +1,51 @@
+<?php namespace App\Services;
+
+use App\User;
+use Validator;
+use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
+
+class Registrar implements RegistrarContract {
+
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	public function validator(array $data)
+	{
+		return Validator::make($data, [
+			'username' => 'required|max:15',
+			'email' => 'required|email|max:255|unique:users',
+			'password' => 'required|confirmed|min:6|max:15',
+			'firstname' => 'required|max:20',
+			'lastname' => 'required|max:30',
+			'sex' => 'required',
+			'tel' => 'required|numeric',
+			'birthdate' => 'required',
+
+		]);
+	}
+
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return User
+	 */
+	public function create(array $data)
+	{
+		return User::create([
+			'username' => $data['name'],
+			'email' => $data['email'],
+			'password' => bcrypt($data['password']),
+			'firstname' => $data['firstname'],
+			'lastname' => $data['lastname'],
+			'nickname' => $data['nickname'],
+			'tel' => $data['tel'],
+			'sex' => $data['sex'],
+			'birthdate' => $data['birthdate'],
+		]);
+	}
+
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('username');
+			$table->string('email')->unique();
+			$table->string('password', 60);
+			$table->string('firstname',20);
+			$table->string('lastname',30);
+			$table->string('nickname',20);
+			$table->string('sex',1);
+			$table->dateTime('birthdate');
+			$table->string('tel',10);
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
