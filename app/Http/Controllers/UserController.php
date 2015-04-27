3fd31db15733208de66242abc90458a0ab81ce5d
ArchSanta <santazaaa@gmail.com>
2015-04-27 22:52:12 +0700

    คอสสส ทำ invite/request หน่อย TT  ทำหน้าชื่อ application ไว้แล้ว อยู่ใน user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,34 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller {
 	
+	public function getApplication($id) {
+		if(!Auth::check()) {
+			return redirect('home');
+		}
+		
+		$user = Auth::user();
+		if($user->id != $id) {
+			return redirect('activity');
+		}
+
+		$applications = \App\Application::with('recruitment')->where('user_id',$user->id)->get();
+
+		$invites = array();
+		$requests = array();
+
+		foreach ($applications as $app) {
+			if($app->sender_flag == 1) {
+				// Invite
+				array_push($invites,$app);
+			} else {
+				// Request
+				array_push($requests,$app);
+			}
+		}
+
+		return view('user.application', compact('applications', 'requests','invites'));
+	}
+
 	public function getProfile($id) {
 		$user = \App\User::find($id);
 		return view('user.profile', compact('user'));
