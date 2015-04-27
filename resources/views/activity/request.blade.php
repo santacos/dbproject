b7c79eab9bae0d31f209bff37ffcb73dd702a51e
Aunaun <dekdekbaloo@gmail.com>
2015-04-28 01:58:35 +0700

    ลบ txt โง่ๆออก เพิ่มหน้า request.blade

---
+++ resources/views/activity/request.blade.php
@@ -0,0 +1,75 @@
+@extends('app')
+
+@section('active_4')
+class="active"
+@endsection
+
+@section('content')
+
+<div class="container">
+	<section class="contest">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="row">
+					<div class="col-xs-9">
+						<h1 style="margin: 0; padding-bottom: 30px;">  Your Activities <small>รายการกิจกรรมทั้งหมดของคุณ</small></h1>
+					</div>
+					<div class="col-xs-3">
+						<a href="{{url('/activity/create')}}" class="btn btn-primary btn-lg" style="position:relative;">สร้างกิจกรรมใหม่</a>
+					</div>
+				</div>
+					<div class="box">
+						@if(count($activities) > 0)
+						<div class="box-body table-responsive no-padding">
+							<table class="table table-bordered table-hover">
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>Name</th>
+										<th>Detail</th>
+										<th>Start Date</th>
+										<th>End Date</th>
+										<th>Affiliation</th>
+										<th>Create by</th>
+										<td>Action</td>
+									</tr>
+								</thead>
+								<?php $i=0 ?>
+								<tbody>
+									@foreach($activities as $activity)
+									<tr>
+										<th scope="row">{{++$i}}</th>
+										<td>{{$activity->name}}</td>
+										<td>{{$activity->detail}}</td>
+										<td>{{$activity->start_date}}</td>
+										<td>{{$activity->end_date}}</td>
+										<td>{{$activity->affiliation->name}}</td>
+										<td>
+											@foreach($activity->user as $man)
+											{{$man->username}} <br>{{$man->email}}
+											@endforeach
+										</td>
+										<td>
+											<a href="{{url('activity/'.$activity->act_id)}}" class="btn btn-info">ดูรายละเอียด</a>
+											<a href="{{url('adddepart/create?id='.$activity->act_id)}}" class="btn btn-info">จัดการ</a>
+										</td>
+									</tr>
+									@endforeach
+								</tbody>
+							</table>
+							@else
+							<div class="callout callout-danger">
+								<h4>คุณยังไม่ได้สร้างกิจกรรมใดๆ</h4>
+								<p>คุณสามารถสร้างกิจกรรมใหม่ขึ้นมาได้ด้วยการกดปุ่มสร้างกิจกรรมใหม่.</p>
+							</div>
+							@endif
+						</div>
+					</div><!-- /.box-body -->
+				</div><!-- /.box -->
+			</div>
+		</div>
+	</section>
+</div>
+
+@endsection
