04e3c82f6d8de9825da8e49da9269bc0605db224
ArchSanta <santazaaa@gmail.com>
2015-04-27 13:14:17 +0700

    - แก้หน้า create index youractivity นิดหน่อย

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -10,15 +10,18 @@ class="active"
 	All activities
 	<small>รายการกิจกรรมทั้งหมดที่สามารถเข้าร่วมได้</small>
 </h1>
+
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="box">
 				<div class="box-header">
 					<h3 class="box-title">Responsive Hover Table</h3>
+					
 					<div class="box-tools">
 						<div class="input-group">
 							<input type="text" name="table_search" class="form-control input-sm pull-right" style="width: 150px;" placeholder="Search">
 							<div class="input-group-btn">
+					
 								<button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
 							</div>
 						</div>
